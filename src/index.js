f05757a656705a6c13468c6f7030e56b92c36d84
AlexGorn0000 <49290929+AlexGorn0000@users.noreply.github.com>
2020-03-21 00:51:24 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import 'core-js/features/map';
 import 'core-js/features/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import bridge from '@vkontakte/vk-bridge';
+import connect from '@vkontakte/vk-connect';
 import App from './App';
-import * as eruda from 'eruda';
-import * as erudaCode from 'eruda-code';
-import * as erudaDom from 'eruda-dom';
+//import registerServiceWorker from './sw';
 
 // Init VK  Mini App
-bridge.send('VKWebAppInit');
+connect.send('VKWebAppInit');
+connect.sendPromise("VKWebAppShowLeaderBoardBox", {user_result:100}) 
+         .then(data => console.log(data.success)) 
+        .catch(error => console.log(error));
+// Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
+// расскомментируйте строку с registerServiceWorker();
+// Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
+// Подробнее про сервис воркеры можно почитать тут — https://vk.cc/8MHpmT
+// registerServiceWorker();
 
 ReactDOM.render(<App />, document.getElementById('root'));
-if (process.env.NODE_ENV === 'development') {
-	eruda.init();
-	eruda.add(erudaCode);
-	eruda.add(erudaDom);
-}
